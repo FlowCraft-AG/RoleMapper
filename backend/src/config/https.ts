14fab19a2f95bdd46d38e0e535b1d3fbc986422f
Caleb-Script <caleb-script@outlook.de>
2024-12-03 22:41:12 +0100

    Update https.ts

--- backend/src/config/https.ts
+++ backend/src/config/https.ts
@@ -7,12 +7,6 @@ import { VOLUMES_DIR } from './app.js';
 const KEYS_DIR = path.resolve(VOLUMES_DIR, 'keys');
 console.debug('tlsDir = %s', KEYS_DIR);
 
-// export const httpsOptionss: HttpsOptions = {
-//     key: readFileSync(path.resolve(KEYS_DIR, 'key.pem'), 'utf8') || process.env.KEY, // eslint-disable-line security/detect-non-literal-fs-filename
-//     cert:
-//         readFileSync(path.resolve(KEYS_DIR, 'certificate.crt'), 'utf8') || process.env.CERTIFICATE, // eslint-disable-line security/detect-non-literal-fs-filename
-// };
-
 export const httpsOptions = {
     key: existsSync(path.resolve(KEYS_DIR, 'key.pem'))
         ? readFileSync(path.resolve(KEYS_DIR, 'key.pem'))
