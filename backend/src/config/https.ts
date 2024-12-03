69172d56dc4dc3e137e18c5bc39af2848b8b083f
Caleb-Script <caleb-script@outlook.de>
2024-12-03 10:59:05 +0100

    Update https.ts

--- backend/src/config/https.ts
+++ backend/src/config/https.ts
@@ -1,13 +1,23 @@
-import { type HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
-import { readFileSync } from 'node:fs';
+/* eslint-disable @eslint-community/eslint-comments/disable-enable-pair */
+/* eslint-disable security/detect-non-literal-fs-filename */
+import { existsSync, readFileSync } from 'node:fs';
 import path from 'node:path';
 import { VOLUMES_DIR } from './app.js';
 
 const KEYS_DIR = path.resolve(VOLUMES_DIR, 'keys');
 console.debug('tlsDir = %s', KEYS_DIR);
 
-export const httpsOptions: HttpsOptions = {
-    key: readFileSync(path.resolve(KEYS_DIR, 'key.pem'), 'utf8') || process.env.KEY, // eslint-disable-line security/detect-non-literal-fs-filename
-    cert:
-        readFileSync(path.resolve(KEYS_DIR, 'certificate.crt'), 'utf8') || process.env.CERTIFICATE, // eslint-disable-line security/detect-non-literal-fs-filename
+// export const httpsOptionss: HttpsOptions = {
+//     key: readFileSync(path.resolve(KEYS_DIR, 'key.pem'), 'utf8') || process.env.KEY, // eslint-disable-line security/detect-non-literal-fs-filename
+//     cert:
+//         readFileSync(path.resolve(KEYS_DIR, 'certificate.crt'), 'utf8') || process.env.CERTIFICATE, // eslint-disable-line security/detect-non-literal-fs-filename
+// };
+
+export const httpsOptions = {
+    key: existsSync(path.resolve(KEYS_DIR, 'key.pem'))
+        ? readFileSync(path.resolve(KEYS_DIR, 'key.pem'), 'utf8')
+        : process.env.KEY,
+    cert: existsSync(path.resolve(KEYS_DIR, 'certificate.crt'))
+        ? readFileSync(path.resolve(KEYS_DIR, 'certificate.crt'), 'utf8')
+        : process.env.CERTIFICATE,
 };
