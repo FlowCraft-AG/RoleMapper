db5ec1c94bfca2e46ca5da504d09427f12c1933c
Caleb-Script <caleb-script@outlook.de>
2024-12-03 22:36:11 +0100

    Update https.ts

--- backend/src/config/https.ts
+++ backend/src/config/https.ts
@@ -1,23 +1,23 @@
 /* eslint-disable @eslint-community/eslint-comments/disable-enable-pair */
 /* eslint-disable security/detect-non-literal-fs-filename */
-import { existsSync, readFileSync } from 'node:fs';
+import { type HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
+import { readFileSync } from 'node:fs';
 import path from 'node:path';
 import { VOLUMES_DIR } from './app.js';
 
 const KEYS_DIR = path.resolve(VOLUMES_DIR, 'keys');
 console.debug('tlsDir = %s', KEYS_DIR);
 
-// export const httpsOptionss: HttpsOptions = {
-//     key: readFileSync(path.resolve(KEYS_DIR, 'key.pem'), 'utf8') || process.env.KEY, // eslint-disable-line security/detect-non-literal-fs-filename
-//     cert:
-//         readFileSync(path.resolve(KEYS_DIR, 'certificate.crt'), 'utf8') || process.env.CERTIFICATE, // eslint-disable-line security/detect-non-literal-fs-filename
-// };
-
-export const httpsOptions = {
-    key: existsSync(path.resolve(KEYS_DIR, 'key.pem'))
-        ? readFileSync(path.resolve(KEYS_DIR, 'key.pem'))
-        : process.env.KEY,
-    cert: existsSync(path.resolve(KEYS_DIR, 'certificate.crt'))
-        ? readFileSync(path.resolve(KEYS_DIR, 'certificate.crt'))
-        : process.env.CERTIFICATE,
+export const httpsOptions: HttpsOptions = {
+    key: readFileSync(path.resolve(KEYS_DIR, 'key.pem')),
+    cert: readFileSync(path.resolve(KEYS_DIR, 'certificate.crt')),
 };
+
+// export const httpsOptions = {
+//     key: existsSync(path.resolve(KEYS_DIR, 'key.pem'))
+//         ? readFileSync(path.resolve(KEYS_DIR, 'key.pem'))
+//         : process.env.KEY,
+//     cert: existsSync(path.resolve(KEYS_DIR, 'certificate.crt'))
+//         ? readFileSync(path.resolve(KEYS_DIR, 'certificate.crt'))
+//         : process.env.CERTIFICATE,
+// };
