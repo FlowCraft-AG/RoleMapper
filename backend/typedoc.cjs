154351ebcf17cc817b15bb56054c0f465928ad67
Caleb-Script <caleb-script@outlook.de>
2024-12-03 14:56:11 +0100

    Update cd-ci-backend.yml

--- backend/typedoc.cjs
+++ backend/typedoc.cjs
@@ -1,11 +1,15 @@
-/* global module */
 /** @type {import('typedoc').TypeDocOptions} */
 module.exports = {
-    out: '../.extras/doc/api',
+    // out: '../.extras/doc/api',
+    out: '../docs/backend',
     entryPoints: ['src'],
     entryPointStrategy: 'expand',
+    excludeExternals: true,
     excludePrivate: true,
+    theme: 'default',
     validation: {
         invalidLink: true,
     },
+    name: 'RoleMapper Backend API Documentation',
+    readme: './README.md',
 };
