ed3ae0ebd93b233e5a53b310fc5bd753380d2bae
Caleb-Script <caleb-script@outlook.de>
2024-12-04 21:23:32 +0100

    Updae Coverage: getDate Resolver

---
+++ backend/__tests__/config/mongo-database.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from '@jest/globals';
+import { ensureEnvironmentVariableDefined } from '../../src/config/mongo-database.js'; // Adjust the import path as necessary
+
+describe('ensureEnvironmentVariableDefined', () => {
+    test('should return the variable if it is defined', () => {
+        const variable = 'defined_variable';
+        const result = ensureEnvironmentVariableDefined(variable, 'TEST_VARIABLE');
+
+        expect(result).toBe(variable);
+    });
+
+    test('should throw an error if the variable is undefined', () => {
+        expect(() => ensureEnvironmentVariableDefined(undefined, 'UNDEFINED_VARIABLE')).toThrow(
+            'Die Umgebungsvariable UNDEFINED_VARIABLE ist nicht definiert. Bitte prüfe deine .env-Datei.',
+        );
+    });
+});
