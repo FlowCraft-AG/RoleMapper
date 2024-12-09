201e1ce570631d098346300e481e5f74e1851b5c
Caleb-Script <caleb-script@outlook.de>
2024-12-09 20:24:25 +0100

    Add OrgUnit.page

---
+++ frontend/src/app/orgUnit/page.tsx
@@ -0,0 +1,15 @@
+import Navigation from "@/src/components/Navigation";
+
+
+export default function OrgPage() {
+  return (
+    <div>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <h3>Organisationseinheiten der Hochschule Karlsruhe</h3>
+      </main>
+    </div>
+  );
+}
