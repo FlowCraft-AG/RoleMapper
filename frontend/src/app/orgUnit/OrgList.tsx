26becd1627c6eb8c3f4dc99be09d691abb427b34
bichanhoang <bichanhoang00@gmail.com>
2024-12-10 17:05:38 +0100

    branch update

--- frontend/src/app/orgUnit/OrgList.tsx
+++ frontend/src/app/orgUnit/OrgList.tsx
@@ -40,3 +40,5 @@ export default function OrgUnits() {
     </div>
   );
 }
+
+
--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -9,7 +9,7 @@ export default function Navigation() {
   return (
     <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>
       <Link href="/" style={{ marginRight: '15px', color: pathname === '/' ? 'gray' : '#fff' }}>Home</Link>
-      <Link href="/orgUnit" style={{ marginRight: '15px', color: pathname === '/orgUnit' ? 'gray' : '#fff' }}>Organisationseinheiten</Link>
+      <Link href="/orgUnit" style={{ marginRight: '15px', color: pathname === '/orgUnit' ? 'gray' : '#fff' }}>Organisationseinheiten</Link> //revalidate einfügen
     </nav>
   );
 }
