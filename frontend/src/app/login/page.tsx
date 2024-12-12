4c6e945ba49b0e483ac5fc1d1b46b8ee37433520
bichanhoang <bichanhoang00@gmail.com>
2024-12-12 15:20:30 +0100

    changed directory for login

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -7,7 +7,7 @@ const LoginPage: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const router = useRouter(); // useRouter-Hook verwenden
+  const router = useRouter();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
