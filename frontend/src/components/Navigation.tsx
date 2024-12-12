c2bdf202279a2ffbedcb33e138e2e84f04f09eb7
bichanhoang <bichanhoang00@gmail.com>
2024-12-12 13:50:56 +0100

    improved navbar

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -7,7 +7,6 @@ export default function Navigation() {
   const pathname = usePathname();
 
   return (
-<<<<<<< HEAD
     <nav
       style={{
         display: 'flex',
@@ -32,11 +31,6 @@ export default function Navigation() {
           </Link>
         )}
       </div>
-=======
-    <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>
-      <Link href="/" style={{ marginRight: '15px', color: pathname === '/' ? 'gray' : '#fff' }}>Home</Link>
-      <Link href="/test_orgUnit" style={{ marginRight: '15px', color: pathname === '/test_orgUnit' ? 'gray' : '#fff' }}>Organisationseinheiten</Link>
->>>>>>> 93666387ec411201222081ccd1c907b209ebb25d
     </nav>
   );
 }
