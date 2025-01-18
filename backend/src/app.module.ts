d991b545923c6e9a62f826f8be4a8dc567553dde
Oliver Nieslony <niol1013@h-ka.de>
2025-01-18 21:19:04 +0100

    reset file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,55 +12,19 @@ import { WriteController } from './role-mapper/controller/write.controller.js';
 import { RoleMapperModule } from './role-mapper/role-mapper.module.js';
 import { KeycloakModule } from './security/keycloak/keycloak.module.js';
 
-/**
- * @file AppModule - Hauptmodul der Anwendung.
- * @description Diese Datei definiert das zentrale Modul der NestJS-Anwendung und konfiguriert wichtige Funktionalitäten wie Middleware, GraphQL, Datenbankverbindungen und weitere Module.
- * @module AppModule
- */
-
 @Module({
     imports: [
-        /**
-         * GraphQL-Modulkonfiguration mit ApolloDriver.
-         * Weitere Optionen werden in der Datei `graphql.js` definiert.
-         */
         GraphQLModule.forRoot<ApolloDriverConfig>(graphQlModuleOptions),
-
-        /**
-         * Logger-Modul, um Anwendungsprotokolle zu verwalten.
-         */
         LoggerModule,
-
-        /**
-         * Keycloak-Modul für Sicherheits- und Authentifizierungsmechanismen.
-         */
         KeycloakModule,
-
-        /**
-         * Mongoose-Modul zur Verbindung mit der MongoDB-Datenbank.
-         */
         MongooseModule.forRoot(database.databaseUri, {
             dbName: database.databaseName,
         }),
-
-        /**
-         * RoleMapper-Modul zur Verwaltung von Rollen- und Berechtigungszuordnungen.
-         */
         RoleMapperModule,
-
-        /**
-         * Zeebe-Modul für die Camunda-Zeebe-Integration.
-         */
-        ZeebeModule,
         CamundaModule,
     ],
 })
 export class AppModule implements NestModule {
-    /**
-     * Konfiguriert Middleware für die Anwendung.
-     * 
-     * @param consumer MiddlewareConsumer - Ermöglicht die Anwendung von Middleware für definierte Routen.
-     */
     configure(consumer: MiddlewareConsumer) {
         consumer
             .apply(RequestLoggerMiddleware)
